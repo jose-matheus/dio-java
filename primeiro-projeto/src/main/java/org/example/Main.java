de3139f084a427c4f71bbbfe0f1e38d7ed1d4924
José Matheus <132094255+jose-matheus@users.noreply.github.com>
2025-04-02 19:58:06 -0300

    exercicio condicional, triangulo valido + classificação dos tipos de triangulos

--- primeiro-projeto/src/main/java/org/example/Main.java
+++ primeiro-projeto/src/main/java/org/example/Main.java
@@ -1,10 +1,45 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Boas Vindas turma DIO");
 
+        Scanner c = new Scanner(System.in);
+        int[] lado = new int[3];
 
+        for (int i = 0; i <= 2; i++) {
+            while(true) {
+                System.out.println("Digite o lado do triangulo");
+                int valor = c.nextInt();
+                if (valor > 0) {
+                    lado[i] = valor;
+                    break;
+                } else {
+                    System.out.println("Você n digitou um lado valido, tente novamente.");
+                }
+            }
+        }
+
+        Arrays.sort(lado);
+
+        c.close();
+
+        if(lado[0] + lado[1] > lado[2]){
+            System.out.println("Você digitou um triangulo valido");
+        }else{
+            System.out.println("Seu triangulo não é valido");
+        }
+
+        if (lado[0] == lado[1] && lado[1] == lado[2]) {
+            System.out.println("Este é um triângulo equilátero.");
+        } else if (lado[0] == lado[1] || lado[1] == lado[2]) {
+            System.out.println("Este é um triângulo isósceles.");
+        } else {
+            System.out.println("Este é um triângulo escaleno.");
+        }
     }
 }
+
