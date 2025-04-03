9cfd4272fea980afb0ec5b7ba593e5d5831f0aea
José Matheus <132094255+jose-matheus@users.noreply.github.com>
2025-04-03 19:44:51 -0300

    Calculo de fatorial, utilizando arraylist para mostrar o fatorial completo e um laço for para realizar o calculo do fatorial

---
+++ CalculoFatorial/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        long resultado = 1;
+        ArrayList<Integer> fator = new ArrayList<Integer>();
+
+        System.out.println("Digite o número que deseja saber o fatorial!");
+        int fatorial = num.nextInt();
+
+        for(int i = fatorial; i  >= 1; i--){
+            resultado *= i;
+            fator.add(i);
+        }
+
+        System.out.println("O fatorial do número é : ".concat(fator.toString()).concat(" = ") + resultado);
+
+    }
+}
